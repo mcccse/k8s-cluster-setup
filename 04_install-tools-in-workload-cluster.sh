f6b4522057b5e7a9ff261a21a23ba58bc937dd27
Lars Kronfält <lakr@mccc.se>
2026-06-06 07:28:08 +0200

    chore: add -y option to say yes to all install

--- 04_install-tools-in-workload-cluster.sh
+++ 04_install-tools-in-workload-cluster.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
-./install_hccm.sh &&
-  ./install_cilium.sh &&
-  ./install_gateway.sh
+YES=0
+if [[ "${1:-}" == "-y" || "${1:-}" == "--yes" ]]; then
+  YES=1
+  shift
+fi
+
+if [[ "$YES" -eq 1 ]]; then
+  yes | ./install_hccm.sh &&
+    yes | ./install_cilium.sh &&
+    yes | ./install_gateway.sh
+else
+  ./install_hccm.sh &&
+    ./install_cilium.sh &&
+    ./install_gateway.sh
+fi
